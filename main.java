6d26582c3a4dc95039e0cbe09cd3d711af7f326f
student <student@STANOWISKO.sala308>
2026-03-26 12:09:37 +0100

    Pierwszy prawdziwy com

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
 class main{
-	public static void main(String args[]){
-		
+	public static void main(String[] args){
+		car ford = new car("ford", "Raptor", 2015);
+		car Toyota = new car("Toyota", "yaris", 2006);
+
+		ford.showinfo();
+		Toyota.showinfo();
 	}
 }
+
+
